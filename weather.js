403f705c396420a28583b1793f01b070326ebd96
unknown <krithikroshan113@gmail.com>
2024-06-19 17:03:47 +0530

    weather

--- weather.js
+++ weather.js
@@ -12,28 +12,29 @@ function data() {
     .then((data) => {
       let country = [],
         flag = [],
-        capital = [],
+        capitals = [],
         regions = [],
         countryCode = [];
       data.map((val) => {
         country.push(val.name.common);
         flag.push(val.flags.svg);
-        capital.push(val.capital);
+        capitals.push(val.capital);
         regions.push(val.region);
-
         countryCode.push(val.cca3);
+        // console.log(val)
       });
       let cards = document.querySelectorAll(".card");
       for (i = 0; i < 100; i++) {
         let h2 = document.createElement("h2");
         let img = document.createElement("img");
         let pCap = document.createElement("p");
-        pCap.innerText = `Captial: ${capital[i]}`;
+        pCap.innerText = `Captial: ${capitals[i]}`;
         let pReg = document.createElement("p");
         pReg.innerText = `Region: ${regions[i]}`;
         let code = document.createElement("p");
         let button = document.createElement("button");
         button.setAttribute("value", `${countryCode[i]}`);
+        button.setAttribute("onclick","clicking(event.target.value)")
         button.innerText = "Click for Weather";
         h2.innerText = country[i];
         code.innerText = `Country Code: ${countryCode[i]}`;
@@ -45,13 +46,16 @@ function data() {
         cards[i].append(code);
         cards[i].append(button);
       }
-     
     })
     .catch((err) => {
       console.log(err);
     });
 }
 data();
+
+function clicking(e) {
+  alert(e)
+}
 
 
 
